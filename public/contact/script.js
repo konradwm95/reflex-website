101d44aa6d5f782ce70743f246fa109c8113dc9b
Konrad <konradwm95@gmail.com>
2022-09-07 18:39:42 +0200

    before creating an universal CSS file

--- public/contact/script.js
+++ public/contact/script.js
@@ -1,3 +1,12 @@
+var map = L.map('map').setView([49.762, 19.089], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+L.marker([49.762, 19.089]).addTo(map)
+.bindPopup("<b>Pracownia Psychologii Transportu</b><br>ul. Wyzwolenia 47, <br>43-365 Wilkowice")
+.openPopup();
+
 const shrinkNavOnScroll = () => {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.querySelector("nav").classList.add("shrinked");
